59ad0de4eab695c64a1e391cc18ff2d2f99ebf88
seghodavid <56871763+seghodavid@users.noreply.github.com>
2023-03-28 17:53:55 +0100

    App.js Setup

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const cors = require("cors")
+const morgan = require("morgan")
+const helmet = require("helmet")
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 
 
 dotenv.config()
 const app = express()
 
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP request to  100 requests per window
+  })
+);
+
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(xss())
+app.use(morgan(dev))
+
+
+app.get('/', (req, res, next) => {
+    res.send("<h1>Welcome to the Scello Assessment homepage</h1>")
+})
 
 const PORT= process.env.PORT || 5001
 
